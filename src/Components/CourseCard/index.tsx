a80ec2550e5d9f3d92080268b1cbcf48fd05c2ab
Komovich <61323536+Komovich@users.noreply.github.com>
2023-04-03 01:07:20 +0300

    fixed TS errors

--- src/Components/CourseCard/index.tsx
+++ src/Components/CourseCard/index.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+<<<<<<< Updated upstream
 import CardMedia from "@mui/material/CardMedia";
 import { Typography, Badge, Rating, Chip, Box } from "@mui/material";
 import Video from "../Video";
+=======
+import { Box } from "@mui/material";
+import CourseMedia from "./CourseMedia"
+import CourseDescription from "./CourseDescription"
+import CourseSkills from "./CourseSkills"
+import CourseTitle from "./CourseTitle"
+import CourseRating from "./CourseRating"
+>>>>>>> Stashed changes
 import { Link } from "react-router-dom";
 
 interface Course {
@@ -27,6 +35,7 @@ interface Props {
 const CourseCard: React.FC<Props> = ({ course }) => {
     const [isPlaying, setIsPlaying] = useState(false);
 
+<<<<<<< Updated upstream
     const handleMouseOver = () => {
         setIsPlaying(true);
     };
@@ -155,6 +164,44 @@ const CourseCard: React.FC<Props> = ({ course }) => {
             </Card>
         </Link>
     );
+=======
+  return (
+    <Link to={`/lesson/${course.id}`}>
+      <Card
+        sx={{
+          maxWidth: "345px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          justifyContent: "space-between",
+          padding: "1rem",
+          border: "1px solid #ccc",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+          marginBottom: "30px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <CourseTitle title={course.title}/>
+          <CourseMedia previewImageLink={course.previewImageLink} courseVideoPreview={course.meta.courseVideoPreview} />
+        </Box>
+        
+        <CardContent sx={{ flexGrow: 1 }}>
+        <CourseDescription description={course.description}/>
+
+        <CourseSkills skills={course.meta.skills}/>
+        </CardContent>
+
+       <CourseRating lessonsCount={course.lessonsCount } rating={course.rating}/>
+      </Card>
+    </Link>
+  );
+>>>>>>> Stashed changes
 };
 
 export default CourseCard;
